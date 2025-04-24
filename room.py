5a4c322c48695d537ff81046611e076855567af4
Zikkey <ZikkeyLS@yandex.ru>
2025-04-24 14:15:36 +0300

    Room transition system

--- room.py
+++ room.py
@@ -3,7 +3,12 @@ import player
 import coords
 
 class Room:
-    def __init__(self, size, graphics):
+    def __init__(self, size, coordinates, graphics):
+        self.coordinates = coordinates
+        self.requireTeleport = False
+        self.prevRoom = None
+        self.teleportCoordinates = [0, 0]
+        
         self.entities = []
         self.size = size
         self.graphics = graphics
@@ -11,10 +16,37 @@ class Room:
         doors = []
 
         self.player = player.Player()
-        self.player.initialize(self.graphics, self, (size[0] // 2) * 30, (size[0] // 2) * 35, self.graphics.images["WarriorIdle"])
+        self.player.initialize(self.graphics, self, (size[0] // 2) * 35, (size[0] // 2) * 35, self.graphics.images["WarriorIdle"])
 
         self.entities.append(self.player)
 
+    def on_enabled(self):
+        if self.prevRoom != None:
+            coordsDiff = [self.prevRoom.coordinates[0] - self.coordinates[0], self.prevRoom.coordinates[1] - self.coordinates[1]]
+            if coordsDiff[0] == -1:
+                self.player.target_x = 35
+            elif coordsDiff[0] == 1:
+                self.player.target_x = (self.size[0] - 1) * 35
+            elif coordsDiff[0] == 0:
+                self.player.target_x = self.prevRoom.player.target_x
+            self.player.x = self.player.target_x
+
+            if coordsDiff[1] == -1:
+                self.player.target_y = 35
+            elif coordsDiff[1] == 1:
+                self.player.target_y = (self.size[1] - 1) * 35            
+            elif coordsDiff[1] == 0:
+                self.player.target_y = self.prevRoom.player.target_y
+            self.player.y = self.player.target_y
+
     def on_update(self):
+        if self.player.target_x == 0:
+            self.requireTeleport = True
+            self.teleportCoordinates = [self.coordinates[0] - 1, self.coordinates[1]]
+
+        if self.player.target_x / 35 == self.size[0]:
+            self.requireTeleport = True
+            self.teleportCoordinates =[self.coordinates[0] + 1, self.coordinates[1]]
+
         for entity in self.entities:
             entity.on_update()       
--- world.py
+++ world.py
@@ -1,11 +1,34 @@
 import room
+import rooms.default_room
+import coords
 
 class World:
     def initialize(self, settings, graphics):
         global grid_size
         grid_size = settings
 
-        self.currentRoom = room.Room(grid_size, graphics)
+        self.rooms = {}
+
+        initialRoom = rooms.default_room.DefaultRoom(grid_size, [5, 5], graphics)
+        self.rooms[coords.to_number([5, 5], 10)] = initialRoom
+
+        secondRoom = room.Room(grid_size, [4, 5], graphics)
+        self.rooms[coords.to_number([4, 5], 10)] = secondRoom
+
+        self.currentRoom = None
+        self.prevRoom = None
+        self.setCurrentRoom(initialRoom)
+    
+    def setCurrentRoom(self, room):
+        self.prevRoom = self.currentRoom
+        self.currentRoom = room
+        self.currentRoom.prevRoom = self.prevRoom
+        self.currentRoom.on_enabled()
 
     def on_update(self):
+        if (self.currentRoom.requireTeleport):
+            self.currentRoom.requireTeleport = False
+            if coords.to_number(self.currentRoom.teleportCoordinates, 10) in self.rooms:
+                self.setCurrentRoom(self.rooms[coords.to_number(self.currentRoom.teleportCoordinates, 10)])
+
         self.currentRoom.on_update()
