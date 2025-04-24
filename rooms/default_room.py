236b6b34640bac9dfb417095810b44cfd673e4b1
Zikkey <ZikkeyLS@yandex.ru>
2025-04-24 16:25:01 +0300

    Tutorial

--- rooms/default_room.py
+++ rooms/default_room.py
@@ -3,3 +3,7 @@ from room import *
 class DefaultRoom(Room):
     def __init__(self, size, coordinates, graphics):
         super().__init__(size, coordinates, graphics)
+
+    def on_update(self):
+        super().on_update()
+        self.graphics.render_text("Двигайтесь до упора влево", (250, 10))
